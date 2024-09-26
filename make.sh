dd2372adf444304db33b9e974d57b04328dcd882
Dalton Luce <daltonluce42@gmail.com>
2024-09-26 16:55:14 -0400

    clean up cv

--- make.sh
+++ make.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# Remove existing PDFs
-rm -f ./assets/dalton_luce_cv.pdf ./assets/dalton_luce_cv.aux ./assets/dalton_luce_cv.log ./assets/dalton_luce_cv.out
-rm -f ./assets/cover_letter.pdf ./assets/cover_letter.aux ./assets/cover_letter.log ./assets/cover_letter.out
-
 # Make resume
 if [ "$1" = "private" ]; then
     pdflatex -jobname=./assets/dalton_luce_cv '\def\private{} \input{cv.tex}'
